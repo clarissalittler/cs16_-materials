998f4cbcd85831a602cef1acbfffb1738fd340f9
left_adjoint <left_adjoint@inconsistentuniverse.space>
2025-08-18 16:19:30 -0700

    Adding text

---
+++ cs161A/codesnips/arrayPrintMyster.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int arr[10] = {10,11,12,13,14,15,16,17,18,19};
+  cout << "The array is " << arr << endl;
+  return 0;
+}
---
+++ cs161A/codesnips/arrayPrintRedux.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int arr[10] = {10,11,12,13,14,15,16,17,18,19};
+  for(int i =0; i<10;i++){
+    cout << "The " << i << "th element is: " << arr[i] << endl;
+  }
+  return 0;
+}
