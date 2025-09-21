24d21647cb893e473d1e015c6a06f7fb5189370e
left_adjoint <left_adjoint@inconsistentuniverse.space>
2025-09-20 17:41:46 -0700

    adding casting and a commenting exercise

---
+++ cs161A/codesnips/cast1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  int num1 = 5;
+  int num2 = 3;
+
+  cout << static_cast<double>(num1)/static_cast<double>(num2) << endl;
+  
+  return 0;
+}
